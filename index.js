a26c01427f4da23cd6429047d00e39c78a26f30a
AnneFiger <annefiger@gmail.com>
2023-03-04 12:13:28 +0000

    implementing hardcoded solution for debugging

--- index.js
+++ index.js
@@ -90,19 +90,19 @@ app.post("/api/shorturl", function(req,res){
 });
 
 //need get route to direct to url
-// app.get("api/shorturl/3", function(req, res){
-//   res.redirect("https://www.metoffice.gov.uk")
-// });
-
-app.get("api/shorturl/:shorturl", function(req, res){
-  const shorturl = req.params.shorturl
-  Shorturl.find({short_url: shorturl})
-  .then((result)=>{
-    const variablemaybeinstringformat = result[original_url]
-    const urltoredirectto = encodeURI(`${variablemaybeinstringformat}`)
-    res.redirect(urltoredirectto);
-  })
+app.get("api/shorturl/3", function(req, res){
+  res.redirect("https://www.metoffice.gov.uk")
 });
+
+// app.get("api/shorturl/:shorturl", function(req, res){
+//   const shorturl = req.params.shorturl
+//   Shorturl.find({short_url: shorturl})
+//   .then((result)=>{
+//     const variablemaybeinstringformat = result[original_url]
+//     const urltoredirectto = encodeURI(`${variablemaybeinstringformat}`)
+//     res.redirect(urltoredirectto);
+//   })
+// });
 
 
 app.listen(port, function() {
